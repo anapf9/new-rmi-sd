f34d2fa8c6f4018877daef8e45aa517bfbd95487
Lorrane <lo.nobrem@gmail.com>
2021-01-11 22:34:27 -0300

    pasta lib

---
+++ src/main/java/br/ufs/dcomp/ExemploRMIJava/funtionsClient.java
@@ -0,0 +1,10 @@
+package br.ufs.dcomp.ExemploRMIJava;
+
+import java.util.*;
+import java.rmi.*;
+public class funtionsClient{
+    public static void main(String args[])throws Exception {
+
+
+
+    }
